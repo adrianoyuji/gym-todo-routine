aa83fd47bb0ea704cda0e1cc545326b61a43b45d
adrianoyuji <adrianoyuji@gmail.com>
2020-09-22 08:53:14 -0400

    file structure and npm installs

---
+++ src/components/App/index.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from "react";
+
+//components
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  return <div className="App"></div>;
+}
+
+export default App;
